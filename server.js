a53139252d82ce69888951a51e33de822caacc60
Sandeep Sinha <sinhasandy345@gmail.com>
2023-01-04 14:12:09 +0530

    change3

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
+const mongoose = require('mongoose');
+mongoose.set('strictQuery',false);
 const dotenv = require("dotenv");
 dotenv.config();
 const express = require('express');
 const app =express();
 const path = require('path');
 const cors = require('cors');
-const DbConnect = require('./config/db');
-DbConnect();
+// const DbConnect = require('./config/db');
 
 const corsOptions = {
     origin:process.env.ALLOWED_CLIENTS.split(',')
@@ -26,6 +27,24 @@ app.use(express.static('public'))
 app.set('views',path.join(__dirname,'/views'));
 app.set('view engine','ejs');
 
-app.listen(PORT,()=>{
-    console.log(`Listening on port ${PORT}`)
+//DBconnect
+const DbConnect = async () =>{
+    try{
+        await mongoose.connect(process.env.MONGO_CONNECTION_URL,
+            {
+                useNewUrlParser:true,
+                UseUnifiedTopology:true,
+            });
+            console.log("Db connected")
+    } catch(err)
+    {
+        console.log(err);
+    }
+}
+
+DbConnect().then(()=>
+{
+    app.listen(PORT,()=>{
+        console.log(`Listening on port ${PORT}`)
+    })
 })
